825a441ffb10d6d44c5fbf0ca3a2a76ef3108497
stevenmcdonald <engineers2526@gmail.com>
2020-03-29 17:59:13 -0500

    Initial commit

---
+++ src/test/java/com/devhubsocial/core/developer/DeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhubsocial.core.developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
